d73795b1f225c6d333746efe70f033b804fe1246
seojin <jseojin0512@gmail.com>
2026-08-27 14:45:32 +0900

    refactor: 자바 switch문 코드 구조 개선

--- JavaClass3/src/Ifexample/SwitchCase3.java
+++ JavaClass3/src/Ifexample/SwitchCase3.java
@@ -2,24 +2,21 @@ package Ifexample;
 
 public class SwitchCase3 {
     public static void main(String[] args) {
-        int month = 10;
+        int month = 2;
 
         int day = switch (month) {
-            case 1 -> 31;
-            case 2 -> 28;
-            case 3 -> 31;
-            case 4 -> 30;
-            case 5 -> 31;
-            case 6 -> 30;
-            case 7 -> 31;
-            case 8 -> 31;
-            case 9 -> 30;
-            case 10 -> 31;
-            case 11 -> 30;
-            case 12 -> 31;
-            default -> 0;
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 2 -> {
+                System.out.println("가장 짧은 달입니다.");
+                yield 28;
+            }
+            case 4, 6, 9, 11 -> 30;
+            default -> {
+                System.out.println("없는 달입니다.");
+                yield 0;
+            }
         };
 
-        System.out.println(month+"월은 총 " + day + "일입니다");
+        System.out.println(month+"월은 총 " + day + "일 입니다");
     }
 }
